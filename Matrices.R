bf4b0e4ba8bc8b98e57acd24c1f72ee2c047c1b5
unknown <christian_joseph7@hotmail.com>
2023-01-10 12:31:27 -0500

    Matrix v2

--- Matrices.R
+++ Matrices.R
@@ -65,10 +65,12 @@ colSums(stocks.matrix)
 rowSums(stocks.matrix)
 colMeans(stocks.matrix)
 rowMeans(stocks.matrix)
+
 #append row matriz 
 FB<-c(150,130,160)
 tech.stocks<-rbind(stocks.matrix,FB)
 tech.stocks
+
 #append column matriz
 avr<-rowMeans(tech.stocks)
 tech.stocks<-cbind(tech.stocks,avr)
@@ -80,3 +82,48 @@ mat[1:2,]
 mat[1:2,1:3]
 mat[1,3]
 mat[,1:2]
+
+#Factores y matrices categoricas
+#nominal->sin orden
+animal<-c("d","c","d","d","c")
+fact.animal<-factor(animal)
+#ordinal->con orden
+temp<-c("cold","med","hot","hot","hot","cold","med")
+fact.temp<-factor(temp,ordered = T, levels = c("cold","med","hot"))
+#summary
+summary(fact.temp)
+summary(temp)
+
+#EJERCICIOS
+#1
+a<-c(1,2,3)
+b<-c(4,5,6)
+mat<-rbind(a,b)
+mat
+
+#2
+mat<-matrix(1:9,nrow = 3)
+mat
+
+#3
+is.matrix(mat)
+
+#4
+mat2<-matrix(1:25,byrow=T,nrow=5)
+mat2
+
+#5
+mat2[2,2:3]
+mat2[3,2:3]
+
+#6
+mat2[4:5,4:5]
+
+#7
+mat2+mat2
+
+#8
+matrix(runif(20), ncol = 5)
+matrix(runif(20), nrow = 4)
+
+help(runif)
